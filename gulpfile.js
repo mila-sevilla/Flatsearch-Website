bb3938be76d89e85437d2b76218ab3ef33501a65
msevilla <m.sevillazhou@gmail.com>
2018-07-22 11:00:05 +0200

    adds autoprefixer and src and build folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
 const gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
 // Static server
-gulp.task('default', function() {
+gulp.task('serve', function() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./src"
         }
     });
-    gulp.watch("**/*.html").on('change', browserSync.reload);
-    gulp.watch("**/*.css").on('change', browserSync.reload);
+    gulp.watch("./src/**/*.html").on('change', browserSync.reload);
+    gulp.watch("./src/**/*.css").on('change', browserSync.reload);
 });
+
+gulp.task('autoprefixer', function() {
+    return gulp.src('./src/**/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('docs'))
+})
+
+gulp.task('copy', function() {
+    return gulp.src('./src/**/*.{html,svg,png,jpg,gif,json,js}')
+        .pipe(gulp.dest('docs'))
+})
+
+gulp.task('default', ['serve']);
+gulp.task('build', ['autoprefixer', 'copy'])
